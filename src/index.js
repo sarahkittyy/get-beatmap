bb1f947285112a936e26462f477bb5269941b521
Sarah Ohlin <sarahohlin1@gmail.com>
2020-01-10 22:20:07 -0500

    added modules and sources

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 let args = process.argv.slice(2);
+let config = require('./config.json');
 
 function getMap(id) {
-	
+	console.log(id);
 }
 
+console.log(config);
 for (let id of args) {
 	getMap(id);
 }
